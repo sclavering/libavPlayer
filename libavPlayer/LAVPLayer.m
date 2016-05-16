dcbf2c58cab01635e563255779aa4d8649948b28
Stephen Clavering <stephen@clavering.me.uk>
2016-05-16 11:50:09 +0100

    livavPlayer -> libavPlayer in comments

--- libavPlayer/LAVPLayer.m
+++ libavPlayer/LAVPLayer.m
@@ -1,14 +1,14 @@
 //  Created by Takashi Mochizuki on 11/06/19.
 //  Copyright 2011 MyCometG3. All rights reserved.
 /*
- This file is part of livavPlayer.
+ This file is part of libavPlayer.
 
- livavPlayer is free software; you can redistribute it and/or modify
+ libavPlayer is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation; either version 2 of the License, or
  (at your option) any later version.
 
- livavPlayer is distributed in the hope that it will be useful,
+ libavPlayer is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
