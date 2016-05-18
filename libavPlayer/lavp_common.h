8b4eda09ec1e9a4c1c0778029a7916659846f0c8
Stephen Clavering <stephen@clavering.me.uk>
2016-05-18 11:12:39 +0100

    shrink lavp_common.h

--- libavPlayer/lavp_common.h
+++ libavPlayer/lavp_common.h
@@ -58,15 +58,6 @@
  A/V sync as SDL does not have hardware buffer fullness info. */
 #define SDL_AUDIO_BUFFER_SIZE 1024
 
-/* no AV sync correction is done if below the minimum AV sync threshold */
-#define AV_SYNC_THRESHOLD_MIN 0.01
-/* AV sync correction is done if above the maximum AV sync threshold */
-#define AV_SYNC_THRESHOLD_MAX 0.1
-/* If a frame duration is longer than this, it will not be duplicated to compensate AV sync */
-#define AV_SYNC_FRAMEDUP_THRESHOLD 0.1
-/* no AV correction is done if too big error */
-#define AV_NOSYNC_THRESHOLD 10.0
-
 /* maximum audio speed change to get correct sync */
 #define SAMPLE_CORRECTION_PERCENT_MAX 10
 
--- libavPlayer/lavp_video.m
+++ libavPlayer/lavp_video.m
@@ -30,6 +30,16 @@
 #import "lavp_util.h"
 
 
+/* no AV sync correction is done if below the minimum AV sync threshold */
+#define AV_SYNC_THRESHOLD_MIN 0.01
+/* AV sync correction is done if above the maximum AV sync threshold */
+#define AV_SYNC_THRESHOLD_MAX 0.1
+/* If a frame duration is longer than this, it will not be duplicated to compensate AV sync */
+#define AV_SYNC_FRAMEDUP_THRESHOLD 0.1
+/* no AV correction is done if too big error */
+#define AV_NOSYNC_THRESHOLD 10.0
+
+
 void video_display(VideoState *is);
 
 double compute_target_delay(double delay, VideoState *is);
