38a8525a22e83330c8d07c6efe79b027357744dd
Stephen Clavering <stephen@clavering.me.uk>
2016-05-24 11:02:46 +0100

    stop setting frame ->sample_aspect_ratio, since we never read it

--- libavPlayer/lavp_video.m
+++ libavPlayer/lavp_video.m
@@ -246,14 +246,8 @@ int get_video_frame(VideoState *is, AVFrame *frame)
         return -1;
 
     if (got_picture) {
-        double dpts = NAN;
-
-        if (frame->pts != AV_NOPTS_VALUE)
-            dpts = av_q2d(is->video_st->time_base) * frame->pts;
-
-        frame->sample_aspect_ratio = av_guess_sample_aspect_ratio(is->ic, is->video_st, frame);
-
         if (frame->pts != AV_NOPTS_VALUE) {
+            double dpts = av_q2d(is->video_st->time_base) * frame->pts;
             double diff = dpts - get_master_clock(is);
             if (!isnan(diff) && fabs(diff) < AV_NOSYNC_THRESHOLD &&
                 diff - is->frame_last_filter_delay < 0 &&
