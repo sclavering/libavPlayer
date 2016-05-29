e884ce574dff06fe5ca8895595997e20415e35e9
Stephen Clavering <stephen@clavering.me.uk>
2016-05-29 14:12:47 +0100

    remove some unused #defines

--- libavPlayer/lavp_video.m
+++ libavPlayer/lavp_video.m
@@ -28,12 +28,6 @@
 #import "LAVPMovie+Internal.h"
 
 
-/* no AV sync correction is done if below the minimum AV sync threshold */
-#define AV_SYNC_THRESHOLD_MIN 0.01
-/* AV sync correction is done if above the maximum AV sync threshold */
-#define AV_SYNC_THRESHOLD_MAX 0.1
-/* If a frame duration is longer than this, it will not be duplicated to compensate AV sync */
-#define AV_SYNC_FRAMEDUP_THRESHOLD 0.1
 /* no AV correction is done if too big error */
 #define AV_NOSYNC_THRESHOLD 10.0
 
