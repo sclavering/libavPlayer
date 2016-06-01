4009e3cb4ddefcab4e34efd4c1ff1fd7afa167e9
Stephen Clavering <stephen@clavering.me.uk>
2016-06-01 17:17:49 +0100

    VideoState: group members more logically

--- libavPlayer/VideoState.h
+++ libavPlayer/VideoState.h
@@ -3,36 +3,32 @@
 // LAVP: in ffplay.c, this is just a struct, but we want to have dispatch_queue_t members, and Xcode says ARC prohibits storing those in a struct.
 @interface VideoState : NSObject {
 @public
-    int64_t audio_callback_time;    /* static int64_t audio_callback_time; */
+    int paused;
+    int playback_speed_percent;
+    int volume_percent;
+    __weak id<LAVPMovieOutput> weak_output;
 
-    AVInputFormat *iformat;
     int abort_request;
     int eof;
-    int paused;
     bool seek_req;
     // xxx eliminate seek_from!
     int64_t seek_from;
     int64_t seek_to;
+    bool is_temporarily_unpaused_to_handle_seeking;
+
+    AVInputFormat *iformat;
     AVFormatContext *ic;
 
     Clock audclk;
-
     Decoder* auddec;
     Decoder* viddec;
-
-    bool is_temporarily_unpaused_to_handle_seeking;
     pthread_cond_t continue_read_thread;
-
-    int playback_speed_percent;
-
     dispatch_queue_t parse_queue;
     dispatch_group_t parse_group;
 
-    __weak id<LAVPMovieOutput> weak_output;
-
     // Audio
 
-    int volume_percent;
+    int64_t audio_callback_time;
     double audio_clock;
     int audio_clock_serial;
     int audio_hw_buf_size;
